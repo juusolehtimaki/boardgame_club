f156e6a0017f675fab8b611a812350b489ac210c
Juuso Lehtimäki <juuso.lehtimaki@edu.turkuamk.fi>
2021-12-08 15:07:18 +0200

    Changing environment name

--- gc_env/bin/activate.fish
+++ gc_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ap/Desktop/game_club/cbc_env"
+set -gx VIRTUAL_ENV "/home/ap/Desktop/game_club/gc_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(cbc_env) "
-            printf "%s%s" "(cbc_env) " (set_color normal)
+        if test -n "(gc_env) "
+            printf "%s%s" "(gc_env) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
